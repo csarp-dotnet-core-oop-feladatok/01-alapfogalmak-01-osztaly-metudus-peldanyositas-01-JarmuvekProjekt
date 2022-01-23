77dee299e042580e46a1d2d5ea64d031bf90ed64
Gyuris Csaba <gycsaba72@gmail.com>
2022-01-23 18:19:52 +0100

    Update Program.cs

--- gyak-01-01-01/Program.cs
+++ gyak-01-01-01/Program.cs
@@ -4,7 +4,7 @@ Péter autóval, Zsófia biciklivel jár munkahelyre.
 Készítsen autó (Car) és bicikli(Bicycle) osztályt! 
 Mindegyik osztály tagjai képes elmenni a munkahelyre és hazajönni a munkahelyről. 
 Írja meg ennek megfelelően a GoToWork és ComingHome metódusokat a polimorfizmus elvét figyelembe véve minkét osztályba! 
-Mindegyik metódus egy megfelelő szöveget írjon ki a képernyőre (pl. „Autó megy munkahelyre.” vagy „Kerékpár megy haza.”!)
+Mindegyik metódus egy megfelelő szöveget írjon ki a képernyőre (pl. „Autó megy munkahelyre.” vagy „Kerékpár megy haza.”)
 A feladat megoldása megfelelő, ha a tesztek lefutnak!
 Példányosítsa Péter két járművét, amely elmegy a munkahelyre, majd visszajön.
 Példányosítsa Péter járművét és Zsófi járművét is! Minkettő menjen el a munkahelyre, majd jöjjön haza!
